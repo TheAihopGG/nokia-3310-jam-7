e1f8d10f15c4cacbdc7cfc9f05504d9b92e32f25
TheAihopGG <theaihopgg@gmail.com>
2025-02-25 20:34:22 +0700

    feat: removed pixel by pixel movement and added classic movement + health signals

--- Scenes/Characters/Player/player.gd
+++ Scenes/Characters/Player/player.gd
@@ -1,8 +1,8 @@
 extends Character
 
 func _process(_delta: float) -> void:
-    move_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-    if move_direction.x > 0 and sprite.flip_h:
-        sprite.flip_h = false
-    elif move_direction.x < 0 and not sprite.flip_h:
-        sprite.flip_h = true
+	move_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if move_direction.x > 0 and sprite.flip_h:
+		sprite.flip_h = false
+	elif move_direction.x < 0 and not sprite.flip_h:
+		sprite.flip_h = true
--- Scenes/Characters/character.gd
+++ Scenes/Characters/character.gd
@@ -1,18 +1,36 @@
 class_name Character extends CharacterBody2D
 
+@export var max_health : int = 3
+@export var speed : float = 5
 
+@onready var health : int = max_health
 @onready var sprite  : Sprite2D = get_node("Sprite2D")
 @onready var tilemap : Node2D = get_tree().current_scene.get_node("TileMap")
 @onready var timer_delay_move : Timer = get_node("TimerDelayMove")
 
 var move_direction : Vector2
+var movement_enabled : bool = true
 
-func move() -> void:
-    global_position += move_direction
-    global_position = global_position.round()
-        
+signal health_reduced(new_health : int)
+signal died()
+
 func _physics_process(_delta: float) -> void:
+    movement()
     move_and_slide()
-    if timer_delay_move.is_stopped():
-        timer_delay_move.start()
-        move()
+
+func movement() -> void:
+    if movement_enabled:
+        velocity = move_direction.normalized() * speed
+
+func reduce_health(count : int) -> void:
+    health += count
+    if health > max_health:
+        health = max_health
+    
+    elif health < 0:
+        health = 0
+    
+    if health == 0:
+        died.emit()
+    
+    health_reduced.emit(health)
