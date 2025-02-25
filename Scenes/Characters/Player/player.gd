ba0d49997644c660db48d65b3d9377b2e73e39f8
TheAihopGG <theaihopgg@gmail.com>
2025-02-25 21:20:46 +0700

    fix: removed can_destruct

--- Scenes/Characters/Player/player.gd
+++ Scenes/Characters/Player/player.gd
@@ -4,24 +4,22 @@ var inventory = {
 	'diamonds': 0
 }
 var nearest_diamond : Diamond
-var can_destruct : bool = false
 
 func _ready() -> void:
 	GlobalVars.player = $"."
 
 func _process(_delta: float) -> void:
-	# print(inventory)
 	move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if move_direction.x > 0 and sprite.flip_h:
 		sprite.flip_h = false
 	elif move_direction.x < 0 and not sprite.flip_h:
 		sprite.flip_h = true
 	
-	if can_destruct and Input.is_action_just_pressed('destruct'):
+	if nearest_diamond and Input.is_action_just_pressed('destruct'):
 		if not nearest_diamond.is_destructed:
 			nearest_diamond._start_destruction()
 	
-	elif can_destruct and Input.is_action_just_released('destruct'):
+	elif nearest_diamond and Input.is_action_just_released('destruct'):
 		if not nearest_diamond.is_destructed:
 			nearest_diamond._stop_destruction()
 
@@ -29,10 +27,8 @@ func _on_destruction_area_body_entered(body: Node2D) -> void:
 	print(body)
 	if body.is_in_group('Diamonds'):
 		nearest_diamond = body
-		can_destruct = true
 
 func _on_destruction_area_body_exited(body: Node2D) -> void:
 	print(body)
 	if body.is_in_group('Diamonds'):
 		nearest_diamond = null
-		can_destruct = false
