8bf9e87b254668cbdb51ff9802b2f4467b9114cd
TheAihopGG <theaihopgg@gmail.com>
2025-02-25 20:48:35 +0700

    feat: global vars player var setter

--- Scenes/Characters/Player/player.gd
+++ Scenes/Characters/Player/player.gd
@@ -1,5 +1,8 @@
 class_name Player extends Character
 
+func _ready() -> void:
+	GlobalVars.player = $"."
+
 func _process(_delta: float) -> void:
 	move_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if move_direction.x > 0 and sprite.flip_h:
