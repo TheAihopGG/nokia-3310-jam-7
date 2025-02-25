e1486aa82632803defe2e71549605671fc80d1d3
TheAihopGG <theaihopgg@gmail.com>
2025-02-25 21:02:18 +0700

    feat: simple inventory

--- Scenes/Characters/Player/player.gd
+++ Scenes/Characters/Player/player.gd
@@ -1,9 +1,14 @@
 class_name Player extends Character
 
+var inventory = {
+	'diamonds': 0
+}
+
 func _ready() -> void:
 	GlobalVars.player = $"."
 
 func _process(_delta: float) -> void:
+	print(inventory)
 	move_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if move_direction.x > 0 and sprite.flip_h:
 		sprite.flip_h = false
