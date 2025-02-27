d117f1b478d64cebdc58d16705249746ca0f0a6b
TheAihopGG <theaihopgg@gmail.com>
2025-02-28 00:21:54 +0700

    feat: player animation sprites

--- Scenes/Characters/character.gd
+++ Scenes/Characters/character.gd
@@ -5,7 +5,7 @@ class_name Character extends CharacterBody2D
 @export var acceleration      : int = 5 # ускорение
 @export var max_speed         : int = 40 
 
-@onready var sprite  : Sprite2D = get_node("Sprite2D")
+@onready var sprite  : AnimatedSprite2D = get_node("AnimatedSprite2D")
 @onready var tilemap : Node2D = get_tree().current_scene.get_node("TileMap")
 
 const FRICTION : float = 0.15
@@ -14,21 +14,21 @@ var move_direction : Vector2
 var movement_enabled : bool = true
 
 func _physics_process(_delta: float) -> void:
-    move_and_slide()
-    move()
-    reverse_sprite()
-    get_input()
-    velocity = lerp(velocity, Vector2.ZERO, FRICTION) 
+	move_and_slide()
+	move()
+	reverse_sprite()
+	get_input()
+	velocity = lerp(velocity, Vector2.ZERO, FRICTION) 
 
 func get_input():
-    pass
-    
+	pass
+	
 func move() -> void:
-    velocity += move_direction * acceleration
-    velocity = velocity.limit_length(max_speed)
+	velocity += move_direction * acceleration
+	velocity = velocity.limit_length(max_speed)
 
 func reverse_sprite() -> void:
-    if move_direction.x > 0 and sprite.flip_h:
-        sprite.flip_h = false
-    elif move_direction.x < 0 and not sprite.flip_h:
-        sprite.flip_h = true
+	if move_direction.x > 0 and sprite.flip_h:
+		sprite.flip_h = false
+	elif move_direction.x < 0 and not sprite.flip_h:
+		sprite.flip_h = true
