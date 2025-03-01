3c27b7052f81a85febe1b840cc340911bb9cb931
SamuelRodriguesMGR <marat78880@gmail.com>
2025-03-01 15:39:09 +0500

    генерация пещер

--- Scenes/Characters/Enemies/Beatle/BeatleFSM.gd
+++ Scenes/Characters/Enemies/Beatle/BeatleFSM.gd
@@ -2,31 +2,31 @@ extends FiniteStateMachine
 
 
 func _init() -> void:
-    _add_state("chase")
-    _add_state("hurt")
-    _add_state("dead")
+	_add_state("chase")
+	_add_state("hurt")
+	_add_state("dead")
 
 func _ready() -> void:
-    set_state(states.chase)
+	set_state(states.chase)
 
 func _state_logic(_delta: float) -> void:
-    if state == states.chase:
-        parent.chase()
-        parent.move()
+	if state == states.chase:
+		parent.chase()
+		parent.move()
 
 func _get_transition() -> int:
-    match state:
-        states.hurt:
-            if not animation_player.is_playing():
-                return states.chase
-    return -1
-    
-    
+	match state:
+		states.hurt:
+			if not animation_player.is_playing():
+				return states.chase
+	return -1
+	
+	
 func _enter_state(_previous_state: int, new_state: int) -> void:
-    match new_state:
-        states.chase:
-            animation_player.play("chase")
-        states.hurt:
-            animation_player.play("hurt")
-        states.dead:
-            animation_player.play("dead")
+	match new_state:
+		states.chase:
+			animation_player.play("chase")
+		states.hurt:
+			animation_player.play("hurt")
+		states.dead:
+			animation_player.play("dead")
--- Scenes/game.gd
+++ Scenes/game.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 
 @onready var tile_map : Node2D = get_node("TileMap")
+@onready var player   : CharacterBody2D = get_node("Player")
 
-
-func _on_threaded_tilemap_gaea_renderer_area_rendered(area: Rect2i) -> void:
-    if area.size:
-        tile_map.generate_chunk()
+func _ready() -> void:
+	player.clear_fog()
+	tile_map.generate_chunk(player.global_position)
